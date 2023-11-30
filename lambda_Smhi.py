4d25de4aaac1d76ddba7e3b2e6ec05506fa44853
Marcus Nilsson <113011450+MarcusNilssonn@users.noreply.github.com>
2023-11-30 19:17:50 +0100

    Add files via upload

---
+++ lambda_Smhi.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+import urllib3
+
+timestreamWrite = boto3.client('timestream-write')
+
+def lambda_handler(event, context):
+   
+    http = urllib3.PoolManager()
+    #Weather data from Uppsala Station
+    res = http.request('GET', 'https://opendata-download-metobs.smhi.se/api/version/latest/parameter/1/station/97510/period/latest-hour/data.json')
+    
+    data = json.loads(res.data.decode('utf-8')) 
+
+    #Get the data from the API response.  
+    records = [
+        {
+            'Dimensions': [
+                {'Name': 'device_id', 
+                'Value': data["station"]["name"]}
+                   
+            ],
+            'MeasureName': 'Temperature outdoor',
+            'MeasureValue': str(data["value"][0]["value"]), 
+            'Time': str(data["value"][0]["date"])  
+        }
+           
+    ]
+    
+    # Send records to Timestream table
+    response = timestreamWrite.write_records(DatabaseName='DatabaseName', TableName='tableName', Records=records)
+    
+    return 'Data successfully inserted into database'
